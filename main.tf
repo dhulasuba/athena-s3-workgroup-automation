8073d5de0962130f3c1767b392cb8a82f819c33e
SidagoudanavarD <dhulagouda.sidagoudanavar@rabobank.nl>
2022-02-10 22:15:50 +0100

    mesg

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@
 #Creates an AWS Athena workgroup
 module "athena_workgroup" {
     source = "git::https://github.com/dhulasuba/athena-s3-workgroup-automation.git"
-    count   = length(var.name)
-    name = var.name[count.index]
+    #count   = length(var.name)
+   # name = var.name[count.index]
+  name="docdb_query_user_business_lending"
     #output_location = var.output_location
     output_location = "s3:// athena-workload-dev"
   }
