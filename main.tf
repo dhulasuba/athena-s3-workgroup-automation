e6f4b78b4fcd855ec68f90338e7a4a513bbf9ac0
dhulasuba <79701674+dhulasuba@users.noreply.github.com>
2022-02-11 08:23:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@
 
 resource "aws_athena_workgroup" "docdb_query_user_business_lending" {
   count = "${length(var.name)}"
-  name = "${element(var.name, count.index)}"
+   name = var.name[count.index]
+#   subnet_id     = var.subnet_ids[count.index]
+#   name = "${element(var.name, count.index)}"
 #   name = var.name
 
   configuration {
