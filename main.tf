5dbf66c1adf416bea9598df0692121205895ffb7
dhulasuba <79701674+dhulasuba@users.noreply.github.com>
2022-02-11 12:03:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_athena_workgroup" "docdb_query_user_business_lending" {
 #   count = "${length(var.name)}"
    count = length(var.name)
    name = var.name[count.index]
+   depends_on = [aws_s3_bucket.buckets]
 #   subnet_id     = var.subnet_ids[count.index]
 #   name = "${element(var.name, count.index)}"
 #   name = var.name
